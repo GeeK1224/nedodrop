4f2ebef9bd18f1646f6d70df4d20c160defcfb82
nedogeek <urunbaev.timur@gmail.com>
2024-03-10 18:44:29 +0500

    chore: forgot to add main.py

--- nedodrop/main.py
+++ nedodrop/main.py
@@ -1,79 +1,5 @@
 import flet as ft
-
-class NedodropApp:
-    def __init__(self, page:ft.Page):
-        self.page = page
-        self.appbar_items = [
-            ft.PopupMenuItem(text='Login'),
-            ft.PopupMenuItem(),
-            ft.PopupMenuItem(text='Settings')
-        ]
-        self.appbar = ft.AppBar(
-
-        )
-        self.on_route_change = self.route_change
-    
-    def initialize(self):
-        self.page.views.append(
-            ft.View(
-                "/",
-                [
-                    # there is should be standard layout
-                    self.appbar,
-                    self.layout
-                ],
-                padding=ft.padding.all(0),
-            )
-        )
-        self.page.update()
-        self.page.go("/")
-    
-    def login(self, e):
-        pass
-
- 
-    def route_change(self, e):
-        troute = ft.TemplateRoute(self.page.route)
-        if troute.match("/"):
-            if self.page.client_storage.get("device_name") is not None:
-                self.layout.set_main_view()
-            else:
-                self.layout.set_registration_view()
-        elif troute.match("/settings"):
-            self.layout.set_settings_view()
-        elif troute.match("/history"):
-            self.layout.set_history_view()
-        elif troute.match("/history/:device_name"):
-            self.layout.set_history_view(troute.device_name)
-        elif troute.match("/search_devices"):
-            self.layout.set_search_devices_view()
-        elif troute.match("share_files_view"):
-            self.layout.set_share_files_view()
-        self.page.update()
-    
-    def set_main_view(self):
-        self.active_view = self.store
-        self.page.update()
-    
-    def set_registration_view(self):
-        self.active_view = self.layout.registration_view
-        self.page.update()
-        
-    def set_settings_view(self):
-        self.active_view = self.layout.registration_view
-        self.page.update()
-    
-    def set_history_view(self):
-        self.active_view = self.layout.registration_view
-        self.page.update()
-
-    def set_search_devices_view(self):
-        self.active_view = self.layout.registration_view
-        self.page.update()
-
-    def set_share_files_view(self):
-        self.active_view = self.layout.registration_view
-        self.page.update()
+import views
 
 
 if __name__ == "__main__":
@@ -84,8 +10,41 @@ if __name__ == "__main__":
         page.padding = 0
         page.bgcolor = "#121212"
         page.theme_mode = 'dark'
-        app = NedodropApp(page)
-        page.add(app)
+
+        using_personal_data = ft.Switch("", value=False)
+        device_name_field = ft.TextField(label="Device name", value=page.client_storage.get("device_name"))
+        
+        def route_change(e):
+            page.views.clear()
+            page.views.append(
+                views.main(device_name="nedogeek", page=page)
+            )
+            if page.route == "/registration":
+                page.views.append(
+                    views.registration(page=page, device_name=device_name_field)
+                )
+            if page.route == "/history":
+                page.views.append(
+                    views.history(page)
+                )
+            if page.route == "/settings":
+                page.views.append(
+                    views.settings(device_name_field=device_name_field, control_element=using_personal_data, page=page)
+                )
+            page.update()
+
+        def view_pop(e):
+            page.views.pop()
+            top_view = page.views[-1]
+            page.go(top_view.route)
+
+        page.add(
+
+        )
+        page.on_route_change = route_change
+        page.on_view_pop = view_pop
+        page.go(page.route)
+
         page.update()
 
     ft.app(target=main)
